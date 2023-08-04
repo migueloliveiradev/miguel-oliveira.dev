e63e751ec3a1e0299bc3973c42d6987b9f6fa1a8
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-08-03 21:15:24 -0300

    Correção modelo

--- migueloliveiradev/Models/Works/Projetos/Image.cs
+++ migueloliveiradev/Models/Works/Projetos/Image.cs
@@ -1,9 +1,16 @@
-﻿namespace migueloliveiradev.Models.Works.Projetos;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace migueloliveiradev.Models.Works.Projetos;
 
 public class Image
 {
     public int Id { get; set; }
     public string Descricao { get; set; }
-    public string Url { get; set; }
+    public string Url { get ; set; }
+    [NotMapped]
+    public string UrlCompleta => $"{Environment.GetEnvironmentVariable("SITE_URL_IMAGES")}/{Url}";
+    [ForeignKey("Projeto")]
+    public int ProjetoId { get; set; }
+
     public virtual Project Projeto { get; set; }
 }
